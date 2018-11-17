b9e96911089bd170941466d7ba42816b8817e782
Monte Lunacek <monte.lunacek@gmail.com>
2018-11-17 08:40:57 -0700

    updating app

--- docs/dist/initial_state.js
+++ docs/dist/initial_state.js
@@ -1,5 +1,5 @@
 var initial_state = {
-    'version': "0.0.2",
+    'version': "0.0.3",
     'url': { 'name': 'windgram', 
              'params': {'id': 'broomfield'} 
     },
@@ -11,27 +11,27 @@ var initial_state = {
     'soundings': {
         'copper': {
             'model': 'nam',
-            'url': 'https://s3-us-west-2.amazonaws.com/meteo-data/sounding_copper.json',
+            'url': 'https://s3-us-west-2.amazonaws.com/meteo-project-data-public/sounding_copper.json',
       
          },
          'boulder': {
             'model': 'nam',
-            'url': 'https://s3-us-west-2.amazonaws.com/meteo-data/sounding_boulder.json',
+            'url': 'https://s3-us-west-2.amazonaws.com/meteo-project-data-public/sounding_boulder.json',
       
          },
          'grand_junction': {
             'model': 'nam',
-            'url': 'https://s3-us-west-2.amazonaws.com/meteo-data/sounding_grand_junction.json',
+            'url': 'https://s3-us-west-2.amazonaws.com/meteo-project-data-public/sounding_grand_junction.json',
       
          },
          'eagle': {
             'model': 'nam',
-            'url': 'https://s3-us-west-2.amazonaws.com/meteo-data/sounding_eagle.json',
+            'url': 'https://s3-us-west-2.amazonaws.com/meteo-project-data-public/sounding_eagle.json',
       
          },
         'broomfield': {
             'model': 'nam',
-            'url': 'https://s3-us-west-2.amazonaws.com/meteo-data/sounding_broomfield.json',
+            'url': 'https://s3-us-west-2.amazonaws.com/meteo-project-data-public/sounding_broomfield.json',
         
         },
     },
